466222116ed560c70f9f5b91c15ddc70d0a5e7dc
Breno Augusto Oliveira Gandolfo <114268603+BrenoAugustoOG@users.noreply.github.com>
2024-05-17 15:57:29 -0300

    Delete 1-Semestre/Herança directory

--- 1-Semestre/Herança/AlunoDePosGraduacao.java
+++
@@ -1,7 +0,0 @@
-public class AlunoDePosGraduacao extends Aluno{
-    char conceito;
-    
-    public AlunoDePosGraduacao(String nome, int idade) {
-        super(nome, idade);
-    }
-}
--- 1-Semestre/Herança/Professor.java
+++
@@ -1,7 +0,0 @@
-public class Professor extends Pessoa{
-    int matricula;
-    public Professor(String nome, int idade) {
-        super(nome, idade);
-    }
-
-}
