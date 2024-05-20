0aa7bbc609ee0a1cb3cf556f5f4079397d4f7ad3
Breno Augusto Oliveira Gandolfo <srbreno7@gmail.com>
2024-05-20 12:36:09 -0300

    polimorfismo poo

---
+++ 1-Semestre/Programação-Orientada-A-Objetos/polimorfismo/exemplo-02/Empregado.java
@@ -0,0 +1,49 @@
+public class Empregado {
+    //Tipo get and setter
+    private int tipo;
+    public int getTipo() {
+        return tipo;
+    }
+    public void setTipo(int tipo) {
+        this.tipo = tipo;
+    }
+    //Salario get and setter
+    private double salario;
+    public double getSalario() {
+        return salario;
+    }
+    public void setSalario(double salario) {
+        this.salario = salario;
+    }
+    //Comissao get and setter
+    private double comissao;
+    public double getComissao() {
+        return comissao;
+    }
+    public void setComissao(double comissao) {
+        this.comissao = comissao;
+    }
+    //Bonus get and setter
+    private double bonus;
+    public double getBonus() {
+        return bonus;
+    }
+    public void setBonus(double bonus) {
+        this.bonus = bonus;
+    }
+    //Horas trabalhadas get and setter
+    private double horasTrabalhadas;
+    public double getHorasTrabalhadas() {
+        return horasTrabalhadas;
+    }
+    public void setHorasTrabalhadas(double horasTrabalhadas) {
+        this.horasTrabalhadas = horasTrabalhadas;
+    }
+    //Valor hora get and setter
+    private double valorHora;
+    public double getValorHora() {
+        return valorHora;
+    }
+    public void setValorHora(double valorHora) {
+        this.valorHora = valorHora;
+    }
---
+++ 1-Semestre/Programação-Orientada-A-Objetos/polimorfismo/exemplo-02/EmpregadoHorista.java
@@ -0,0 +1,9 @@
+public class EmpregadoHorista extends Empregado{
+    private double valorHora;
+    private int numeroHoras;
+    
+    @Override
+    public double calculaSalario(){
+        return this.valorHora*this.numeroHoras;
+    }
+}
