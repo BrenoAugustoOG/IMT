fd1ca486f36e4036418fceb47438831a79cb6875
Breno Augusto Oliveira Gandolfo <srbreno7@gmail.com>
2024-10-03 09:11:20 -0300

    aula de Desebvolvimento Front End

---
+++ 2-Semestre/desenvolvimento_front_end/projeto-fullstack/index.js
@@ -0,0 +1,6 @@
+// console.log("Hello, world!!!")
+const express = require('express')
+const app = express()
+app.use(express.json())
+
+app.listen (3000, () => console.log("server up and running"))
