b7d79a030ea476d08af3719beba416542837aceb
Breno Augusto Oliveira Gandolfo <srbreno7@gmail.com>
2024-10-17 09:13:16 -0300

    aula de Desenvolvimento Front End

--- 2-Semestre/desenvolvimento_front_end/projeto-fullstack/front/js/script.js
+++ 2-Semestre/desenvolvimento_front_end/projeto-fullstack/front/js/script.js
@@ -20,6 +20,25 @@ async function obterFilmes() {
         celulaTitulo.innerHTML = filme.titulo
         celulaSinopse.innerHTML = filme.sinopse
     }
-    
-    // console.log(filmes)
+}
+
+async function cadastrarFilme() {
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    let tituloInput = document.querySelector('#tituloInput')
+    let sinopseInput = document.querySelector('#sinopseInput')
+    let titulo = tituloInput.value
+    let sinopse = sinopseInput.value
+    tituloInput.value = ""
+    sinopseInput.value = ""
+    const filmes = (await axios.post(URLcompleta, {titulo, sinopse})).data
+    let tabela = document.querySelector('.filmes')
+    let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
+    for(let filme of filmes) {
+        let linha = corpoTabela.insertRow(0)
+        let celulaTitulo = linha.insertCell(0)
+        let celulaSinopse = linha.insertCell(1)
+        celulaTitulo.innerHTML = filme.titulo
+        celulaSinopse.innerHTML = filme.sinopse
+    }
 }
