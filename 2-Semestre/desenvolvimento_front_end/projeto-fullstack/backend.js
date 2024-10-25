ea2eab7d582492127d6ef047c89a3143c6029fe1
Breno Augusto Oliveira Gandolfo <srbreno7@gmail.com>
2024-10-25 12:51:38 -0300

    aula de Desenvolvimento Front End

--- 2-Semestre/desenvolvimento_front_end/projeto-fullstack/backend.js
+++ 2-Semestre/desenvolvimento_front_end/projeto-fullstack/backend.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+const bcrypt = require('bcrypt')
+
 const app = express()
 app.use(express.json())
 app.use(cors())
@@ -8,6 +11,14 @@ app.use(cors())
 const Filme = mongoose.model("Filme", mongoose.Schema({
     titulo: {type: String}, sinopse: {type: String}
 }))
+
+const usuarioSchema = mongoose.Schema({
+    login: {type: String, required: true, unique: true},
+    password: {type: String, required: true, unique: true}
+})
+
+usuarioSchema.plugin(uniqueValidator)
+const Usuario = mongoose.model ("Usuario", usuarioSchema)
 
 async function conectarAoMongo() {
     await mongoose.connect(`mongodb+srv://srbreno7:170406Ba@cluster.hb9ue.mongodb.net/?retryWrites=true&w=majority&appName=Cluster`)
@@ -25,6 +36,34 @@ app.post('/filmes', async (req,res) => {
     await filme.save()
     const filmes = await Filme.find()
     res.json(filmes)
+})
+
+app.post('/signup', async (req, res) => {
+    try {
+        const login = req.body.login
+        const password = req.body.password
+        const password_criptografada = await bcrypt.hash(password, 10)
+        const usuario = new Usuario({login: login, password: password_criptografada})
+        const respMongo = await usuario.save()
+        console.log(respMongo)
+        res.status(201).end()
+    }
+    catch (e) {
+        console.log('Erro ao efetuar login', e)
+        res.status(409).end()
+    }
+    
+})
+
+app.post('/login', async (req, res) => {
+    const login = req.body.login
+    const password = req.body.password
+    //verifica se o usuário existe
+    const usuarioExiste = await Usuario.findOne({login: login})
+    if (!usuarioExiste) {
+        return res.status(401).json({mensagem: "Login Inválido"})
+    }
+    // Verificar senha e dar continuidade
 })
 
 app.listen (3000, () => {
