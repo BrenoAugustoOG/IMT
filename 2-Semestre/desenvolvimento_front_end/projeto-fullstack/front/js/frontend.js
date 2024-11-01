42e1df500e4910db40775a9a7812101b62bd9e6e
Breno Augusto Oliveira Gandolfo <srbreno7@gmail.com>
2024-11-01 12:51:03 -0300

    aula de Desenvolvimento Front End

--- 2-Semestre/desenvolvimento_front_end/projeto-fullstack/front/js/frontend.js
+++ 2-Semestre/desenvolvimento_front_end/projeto-fullstack/front/js/frontend.js
@@ -1,40 +1,42 @@
 const protocolo = 'http://'
 const baseURL = 'localhost:3000'
-const filmesEndpoint = '/filmes'
 
-async function obterFilmes() {
-    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
-    const filmes = (await axios.get(URLcompleta)).data
-    
+function exibirFilmes(filmes) {
+    const filmesEndpoint = '/filmes'
     let tabela = document.querySelector('.filmes')
     let corpoTabela = tabela.getElementsByTagName('tbody')[0]
+    corpoTabela.innerHTML = ""
     for (let filme of filmes) {
-        // Para cada filme, inserir uma linha
         let linha = corpoTabela.insertRow(0)
-        
-        // Para cada linha, inserir duas células
         let celulaTitulo = linha.insertCell(0)
         let celulaSinopse = linha.insertCell(1)
-        
-        // Atribui o html para cada célula
         celulaTitulo.innerHTML = filme.titulo
         celulaSinopse.innerHTML = filme.sinopse
     }
 }
 
+
+async function obterFilmes() {
+    const filmesEndpoint = '/filmes'
+    const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
+    const filmes = (await axios.get(URLcompleta)).data
+    exibirFilmes(filmes)
+}
+
 async function cadastrarFilme() {
+    const filmesEndpoint = '/filmes'
     const URLcompleta = `${protocolo}${baseURL}${filmesEndpoint}`
     let tituloInput = document.querySelector('#tituloInput')
     let sinopseInput = document.querySelector('#sinopseInput')
-    let titulo = tituloInput.value
+    let titulo = tituloInput.value 
     let sinopse = sinopseInput.value
     tituloInput.value = ""
     sinopseInput.value = ""
     if (titulo && sinopse) {
-        const filmes = (await axios.post(URLcompleta, { titulo, sinopse })).data
+        const filmes = (await axios.post (URLcompleta, {titulo, sinopse})).data
         exibirFilmes(filmes)
     }
-    else{
+    else {
         let alert = document.querySelector('.alert')
         alert.classList.add('show')
         alert.classList.remove('d-none')
@@ -43,5 +45,25 @@ async function cadastrarFilme() {
             alert.classList.add('d-none')
         }, 2000)
     }
+}
 
+async function cadastrarUsuario() {
+    // const cadastrarUsuarioEndpoint = '/signup'
+    let usuarioCadastroInput = document.querySelector('#usuarioCadastroInput')
+    let passwordCadastroInput = document.querySelector('#passwordCadastroInput')
+    let usuarioCadastro = usuarioCadastroInput.value
+    let passwordCadastro = passwordCadastroInput.value
+    if (usuarioCadastro && passwordCadastro) {
+        
+    }
+    else {
+        let alert = document.querySelector('.alert-modal-cadastro')
+        alert.innerHTML = "Preencha todos os campos!"
+        alert.classList.add('show', 'alert-danger')
+        alert.classList.remove('d-none')
+        setTimeout(() =>{
+            alert.classList.remove("show")
+            alert.classList.add('d-none')
+        }, 2000)
+    }
 }
