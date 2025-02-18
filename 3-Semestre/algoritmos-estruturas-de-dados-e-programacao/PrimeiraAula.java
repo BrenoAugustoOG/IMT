4c3bfd7f70a05a2f606812e1d56280496064ddf8
Breno Augusto Oliveira Gandolfo <srbreno7@gmail.com>
2025-02-18 11:09:54 -0300

    aula de Algoritmos

--- 3-Semestre/algoritmos-estruturas-de-dados-e-programacao/PrimeiraAula.java
+++
@@ -1,6 +0,0 @@
-public class PrimeiraAula {
-    public static void main(String[] args) {
-        System.out.println("Só pra salvar a pasta, mas veremos conteúdos em java e c");
-    }
-    
-}
---
+++ 3-Semestre/algoritmos-estruturas-de-dados-e-programacao/nosso_vetor/Teste.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class Teste {
+    public static void main(String[] args) {
+        int i = 10;
+        // int[] v;
+        // System.out.println("Qual o tamanho do vetor?");
+        // Scanner scanner = new Scanner(System.in);
+        // i = scanner.nextInt();
+        // v = new int[i];
+        // for (int j = 0; j < 100; j++)
+        //     v[j] = j;
+        // scanner.close();
+        System.out.println(i == 10);
+    }
+}
